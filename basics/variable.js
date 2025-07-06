ffd50182603d5007f326d0e3ec86b8a5e578b7b4
TEAMOS-PC\Siam <f>
2025-07-07 02:42:13 +0530

    variable

---
+++ basics/variable.js
@@ -0,0 +1,13 @@
+// const => value is not changeable 
+// let => value is changeable
+// var => value is changeable, but not recommended to use
+
+const number = 10;
+let name = "Siam";
+var age = 30;
+
+console.log (number);
+console.log (name);
+console.log (age);
+
+// to run this file, use the command: node variable.js
