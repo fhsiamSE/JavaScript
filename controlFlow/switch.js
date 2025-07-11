b1df17be9207d1635b9e918151ce4c77af0434b7
TEAMOS-PC\Siam <f>
2025-07-11 18:29:10 +0530

    switch statement

---
+++ controlFlow/switch.js
@@ -0,0 +1,43 @@
+// switch statement example in JavaScript
+const month = 'January';
+switch (month) {
+  case 'January':
+    console.log('It is the first month of the year.');
+    break;
+  case 'February':
+    console.log('It is the second month of the year.');
+    break;
+    case 'March':
+    console.log('It is the third month of the year.');
+    break;
+    case 'April':
+    console.log('It is the fourth month of the year.');
+    break;
+    case 'May':
+    console.log('It is the fifth month of the year.');
+    break;
+    case 'June':
+    console.log('It is the sixth month of the year.');
+    break;
+    case 'July':
+    console.log('It is the seventh month of the year.');
+    break;
+    case 'August':
+    console.log('It is the eighth month of the year.');
+    break;
+    case 'September':
+    console.log('It is the ninth month of the year.');
+    break;
+    case 'October':
+    console.log('It is the tenth month of the year.');
+    break;
+    case 'November':
+    console.log('It is the eleventh month of the year.');
+    break;
+    case 'December':
+    console.log('It is the twelfth month of the year.');
+    break;
+    default:
+    console.log('Invalid month');
+    break;
+}
